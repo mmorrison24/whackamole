8c55653e1b8e77892deca25e76439f11d645613c
Mathew Morrison <m@MacBook-Pro.local>
2019-08-28 09:48:04 -0400

    Setup::Firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
+import firebase from "./firebase/firebase";
+
+
 function App() {
+
+  const [user, setUser] = useState(false);
+
+  firebase.auth().onAuthStateChanged(user => {
+    console.log(user);
+    setUser(user);
+  });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
---
+++ src/firebase/firebase.js
@@ -0,0 +1,21 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCuuu2MXHLtmCQ8LD1CUXOmCq7ZAY38cnQ",
+    authDomain: "prideconcept.firebaseapp.com",
+    databaseURL: "https://prideconcept.firebaseio.com",
+    projectId: "prideconcept",
+    storageBucket: "",
+    messagingSenderId: "30021662412",
+    appId: "1:30021662412:web:84c235edb01657b2"
+};
+firebase.initializeApp(firebaseConfig);
+
+export const firestore = firebase.firestore();
+
+export default firebase;
